f19b7ffeacf52c9540a8c743c1f9856a1f56324a
Diego Alonso Ojeda Medina <dojedam@sena.edu.co>
2020-09-18 10:16:50 -0500

    Segunda clase

--- server/app.py
+++ server/app.py
@@ -1,8 +1,18 @@
 from flask import Flask,request,jsonify,abort
+from firebase_admin import credentials, firestore, initialize_app
 import sys
 from database import *
+from models import Estudiante, Peticion
 
 app = Flask(__name__)
+
+# Inicializar Firestore DB
+
+cred = credentials.Certificate('server/key/key.json')
+default_app = initialize_app(cred)
+db = firestore.client()
+estudiantes_ref = db.collection('Estudiantes')
+peticiones_ref = db.collection('Peticiones')
 
 @app.route("/")
 def hola_mundo():
@@ -21,63 +31,81 @@ def despedida(nombre):
   frase = request.args.get('frase', 'Adios')
   return f"{frase} {nombre}!"
 
-# http://127.0.0.1:5000/api/estudiantes/11234224
-@app.route("/api/estudiantes/<int:id>",methods=['GET'])
-def obtener_estudiante(id):
-  try:
-    estudiante=estudiantes_db[str(id)]
-    return jsonify(estudiante),200
-  except:
-    error_message={"error":f"No se encontró ningún estudiante con el ID {id}"}
-    return jsonify(error_message),400
-
-# http://127.0.0.1:5000/api/estudiantes
-@app.route("/api/estudiantes",methods=['GET'])
-def obtener_estudiantes():
-  lista_estudiantes=[estudiantes_db[key] for key in estudiantes_db.keys()]
-  return jsonify(lista_estudiantes),200
-
 # http://127.0.0.1:5000/api/estudiantes
 @app.route("/api/estudiantes",methods=['POST'])
 def agregar_estudiante():
-  estudiante_id=str(request.json["cedula"])
-  if(estudiante_id in estudiantes_db):
-    error_message={"error":f"Ya existe un estudiante registrando con el ID {estudiante_id}"}
-    return jsonify(error_message),400
-    
-  try:  
-    nuevo_estudiante=dict()
-    nuevo_estudiante["cedula"]=request.json["cedula"]
-    nuevo_estudiante["nombre"]=request.json["nombre"]
-    nuevo_estudiante["apellido"]=request.json["apellido"]
-    nuevo_estudiante["carrera"]=request.json["carrera"]
-    nuevo_estudiante["correo"]=request.json["correo"]
-    estudiantes_db[estudiante_id]=nuevo_estudiante
+  try:
+    data=request.json
+    estudiante_id=str(request.json["cedula"])
+    estudiantes_doc=estudiantes_ref.document(estudiante_id)
+    if(estudiantes_doc.get().exists):
+      error_message={"error":f"Ya existe un estudiante registrando con el ID {estudiante_id}"}
+      return jsonify(error_message),400
+
+    nuevo_estudiante = Estudiante(
+                        data["cedula"], 
+                        data["nombre"], 
+                        data["apellido"],
+                        data["correo"], 
+                        data["carrera"]).to_dict()
+    estudiantes_ref.document(estudiante_id).set(nuevo_estudiante)
     return jsonify(nuevo_estudiante),201
   except:
       error_message={"error":"Los datos del estudiante no están completos o son incorrectos"}
       return jsonify(error_message),400
 
+
 # http://127.0.0.1:5000/api/estudiantes/11234224
 @app.route("/api/estudiantes/<string:id>",methods=['PUT'])
 def actualizar_estudiante(id):
-    if(id in estudiantes_db):
-      estudiantes_db[str(id)] = request.json
-      return jsonify(estudiantes_db[str(id)]), 200
+    data=request.json
+    estudiante_doc=estudiantes_ref.document(str(id)).get()
+    if(estudiante_doc.exists):
+      data_estudiante = Estudiante(
+                    data["cedula"], 
+                    data["nombre"], 
+                    data["apellido"],
+                    data["correo"], 
+                    data["carrera"]).to_dict()
+      estudiante_doc=estudiantes_ref.document(str(id)).set(data)
+      return jsonify(data_estudiante), 200
     else:
-      error_message={"error":f"No existe un estudiante registrando con el ID {estudiante_id}"}
+      error_message={"error":f"No existe un estudiante registrando con el ID {estudiante_doc}"}
       return jsonify(error_message),400
 
 # http://127.0.0.1:5000/api/estudiantes/11234224
 @app.route("/api/estudiantes/<string:id>",methods=['DELETE'])
 def eliminar_estudiante(id):
-    if(id in estudiantes_db):
-      del estudiantes_db[str(id)]
+    estudiante = estudiantes_ref.document(str(id)).get()
+    if(estudiante.exists):
+      estudiantes_ref.document(str(id)).delete()
       result={ "cedula":id , "borrado" : True}
       return jsonify(result), 200
     else:
       error_message={"error":f"No se encontró ningún estudiante con el ID {id}"}
       return jsonify(error_message),400
+
+# http://127.0.0.1:5000/api/estudiantes/11234224
+@app.route("/api/estudiantes/<int:id>",methods=['GET'])
+def obtener_estudiante(id):
+    estudiante_doc=estudiantes_ref.document(str(id)).get()
+    if(estudiante_doc.exists):
+      return jsonify(estudiante_doc.to_dict()),200
+    else:
+      error_message={"error":f"No se encontró ningún estudiante con el ID {id}"}
+      return jsonify(error_message),400
+
+
+# http://127.0.0.1:5000/api/estudiantes
+@app.route("/api/estudiantes",methods=['GET'])
+def obtener_lista_estudiantes():
+    results=estudiantes_ref.stream()
+    lista_estudiantes=[item.to_dict() for item in results]
+
+    return jsonify({"data":lista_estudiantes}),200
+  # lista_estudiantes=[estudiantes_db[key] for key in estudiantes_db.keys()]
+  # return jsonify(lista_estudiantes),200
+
 
 if __name__ == '__main__':
     app.run(debug=True)
---
+++ server/models/estudiante.py
@@ -0,0 +1,15 @@
+import pytz
+from datetime import datetime
+
+timezone = pytz.timezone("America/Bogota")
+
+class Estudiante:
+    def __init__(self, cedula, nombre, apellido, correo, carrera):
+        self.cedula = cedula
+        self.nombre = nombre
+        self.apellido = apellido
+        self.correo = correo
+        self.carrera = carrera
+        
+    def to_dict(self):
+        return dict((key, value) for (key, value) in self.__dict__.items())
